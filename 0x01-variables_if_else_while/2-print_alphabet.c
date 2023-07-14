cd42c15c0734bfd6921175307e5cb0da7f2be8e6
safi222 <safia2000gibril@gmail.com>
2023-07-14 10:24:01 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 /**
- * main -Prints aphapet in lowercase
- *
- * Return:Always 0
- */
+* main - Prints the alphabet.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	char alph[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 'e', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
-	for (i = 0; i < 6; i++)
+	for (i = 0; i < 26; i++)
 	{
-		putchar (alph[i]);
+		putchar(alp[i]);
 	}
+		putchar('\n');
 		return (0);
 }
