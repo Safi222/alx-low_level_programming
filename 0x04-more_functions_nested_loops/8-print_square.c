ac978ad3b86aa55b4b053c4106914f52875a65b1
safi222 <safia2000gibril@gmail.com>
2023-07-21 07:06:14 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,13 +11,14 @@ void print_square(int size)
 	} else
 	{
 		int i, j;
+
 		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
 			{
-				_putchar(/'#');
+				_putchar('#');
 			}
-				_putchar('\n');
+			_putchar('\n');
 		}
 	}
 }
