605f221123272ab064e9ffb768ec6297efdc4866
safi222 <safia2000gibril@gmail.com>
2023-08-15 09:16:24 -0700

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * Return: nothing
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,4 @@
+#ifndef SUM_NUM
+#define SUM_NUM
+#define SUM(X, Y) ((X) + (Y))
+#endif
