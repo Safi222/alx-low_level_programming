b8ab01f71e551947513574e910f6254ecdebe415
safi222 <safia2000gibril@gmail.com>
2023-12-25 08:54:07 -0800

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,52 +1,87 @@
-#include "main.h"
 /**
- * infinite_add - add
- * @n1: array 1 pointer
- * @n2: array 2 poimter
- * @r: result array
- * @size_r: size array r
- * Return: pointer to result
+ * calc_len - calculates the length of a string
+ * @str: The string to calculate its length
+ *
+ * Return: The length of the string
+ */
+int calc_len(char *str)
+{
+	int len = 0;
+
+	while (*str++)
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * add_strings - Adds the numbers stored in two strings.
+ * @n1: The string containing the first number to be added.
+ * @n2: The string containing the second number to be added.
+ * @r: The buffer to store the result.
+ * @r_index: The current index of the buffer.
+ *
+ * Return: If r can store the sum - a pointer to the result.
+ *	   If r cannot store the sum - 0.
+ */
+char *add_strings(char *n1, char *n2, char *r, int r_index)
+{
+	int sum, carry = 0;
+
+	for (; *n1 && *n2; n1--, n2--, r_index--)
+	{
+		sum = (*n1 - '0') + (*n2 - '0') + carry;
+		*(r + r_index) = (sum % 10) + '0';
+		carry = sum / 10;
+	}
+
+	for (; *n1; n1--, r_index--)
+	{
+		sum = (*n1 - '0') + carry;
+		*(r + r_index) = (sum % 10) + '0';
+		carry = sum / 10;
+	}
+	for (; *n2; n2--, r_index--)
+	{
+		sum = (*n2 - '0') + carry;
+		*(r + r_index) = (sum % 10) + '0';
+		carry = sum / 10;
+	}
+	if (carry && r_index >= 0)
+	{
+		*(r + r_index) = (carry % 10) + '0';
+		return (r + r_index);
+	}
+	else if (carry && r_index < 0)
+	{
+		return (0);
+	}
+	return (r + r_index + 1);
+}
+/**
+ * infinite_add - Adds two numbers.
+ * @n1: The first number to be added.
+ * @n2: The second number to be added.
+ * @r: The buffer to store the result.
+ * @size_r: The buffer size.
+ *
+ * Return: If r can store the sum - a pointer to the result.
+ *	   If r cannot store the sum - 0.
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
+	int n1_len = calc_len(n1);
+	int n2_len = calc_len(n2);
 
-	while (*(n1 + c1) != '\0')
-		c1++;
-	while (*(n2 + c2) != '\0')
-		c2++;
-	if (c1 >= c2)
-		bg = c1;
-	else
-		bg = c2;
-	if (size_r <= bg + 1)
-		return (0);
-	r[bg + 1] = '\0';
-	c1--, c2--, size_r--;
-	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
-	while (bg >= 0)
+	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
 	{
-		op = dr1 + dr2 + add;
-		if (op >= 10)
-			add = op / 10;
-		else
-			add = 0;
-		if (op > 0)
-			*(r + bg) = (op % 10) + 48;
-		else
-			*(r + bg) = '0';
-		if (c1 > 0)
-			c1--, dr1 = *(n1 + c1) - 48;
-		else
-			dr1 = 0;
-		if (c2 > 0)
-			c2--, dr2 = *(n2 + c2) - 48;
-		else
-			dr2 = 0;
-			bg--, size_r--;
+		return (0);
 	}
-	if (*(r) == '0')
-		return (r + 1);
-	else
-		return (r);
+
+	n1 += n1_len - 1;
+	n2 += n2_len - 1;
+	*(r + size_r) = '\0';
+
+	return (add_strings(n1, n2, r, --size_r));
 }
